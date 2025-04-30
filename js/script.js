b5b76ecfb0a6a8b17c4fb3c409d5b99588c7648a
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-04-30 21:06:28 +0000

    fixed js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yi.zhou
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+/**
+ * * This function checks if the user has selected the random number from above
+*/
+// eslint-disable-next-line no-unused-vars
+function checkStudentPrise() {
+  // input
+  const userage = parseInt(document.getElementById('age').value)
+  const visitTime = document.getElementById('the-day').value.trim()
+  let message=""
+
+  // process
+  
+  if ((userage >= 13 && userage <= 20) && (visitTime === "Thursday" || visitTime === "Tuesday"))  {
+    document.getElementById('prise').innerHTML =
+      '<p>You are elligble for student pricing </p>'
+  } else {
+    document.getElementById('prise').innerHTML =
+      '<p>You must pay regular price</p>'
+  }
+}
